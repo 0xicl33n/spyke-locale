8871730cc834892a2232d2dcccdf4a1f24e730f4
Chris <0xicl33n@gmail.com>
2018-05-30 13:42:02 -0700

    ill work in en later

---
+++ locale/localconf.py
@@ -0,0 +1,6 @@
+import i18n
+i18n.load_path.append('/home/chris/code/murch/cogs/lib/locale')
+i18n.set('fallback', 'en')
+i18n.set('locale', 'es')
+profile_help = i18n.t('profile.profile_help')
+level_help = i18n.t('profile.level_help')
