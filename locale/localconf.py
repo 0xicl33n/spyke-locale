a69258ca1c4be0f5968a8224f2ce679bb74a22b6
Chris <0xicl33n@gmail.com>
2018-05-30 15:03:03 -0700

    i18n yesssssss

--- locale/localconf.py
+++ locale/localconf.py
@@ -1,4 +1,4 @@
 import i18n
-i18n.load_path.append('/home/chris/code/murch/cogs/lib/locale/locale')
+i18n.load_path.append('cogs/lib/locale/locale')
 profile_help = i18n.t('profile.profile_help')
 level_help = i18n.t('profile.level_help')
