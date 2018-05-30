938f5dbaee4e18588869679bd817b2656e37b92d
Chris <0xicl33n@gmail.com>
2018-05-30 15:01:29 -0700

    IDIOTO

--- locale/localconf.py
+++ locale/localconf.py
@@ -1,6 +1,4 @@
 import i18n
-i18n.load_path.append('/home/chris/code/murch/cogs/lib/locale')
-i18n.set('fallback', 'en')
-i18n.set('locale', 'es')
+i18n.load_path.append('/home/chris/code/murch/cogs/lib/locale/locale')
 profile_help = i18n.t('profile.profile_help')
 level_help = i18n.t('profile.level_help')
