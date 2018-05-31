2b30b21a9479de11202025e6e6c28e02a5b172f6
Chris <0xicl33n@gmail.com>
2018-05-31 10:51:16 -0700

    add skeletons

--- locale/localconf.py
+++ locale/localconf.py
@@ -1,6 +1,17 @@
 import i18n
-i18n.load_path.append('cogs/lib/locale/locale')
 
+supported_lang = [
+    'en',
+    'jp',
+    'fr',
+    'de',
+    'es',
+    'zh-cn', #mandarin
+    'zh-tw', #cantonese
+]
+
+i18n.load_path.append('cogs/lib/locale/locale')
+#profile
 profile_help = i18n.t('profile.profile_help')
 level_help = i18n.t('profile.level_help')
 level_set = i18n.t('profile.level_set')
