d6172b491434882b9a5a2ebbd53a68e18abd3bc4
Chris <0xicl33n@gmail.com>
2018-08-05 17:13:09 -0700

    WHAT

---
+++ README.md
@@ -0,0 +1,23 @@
+
+1) Create a new file in the `locale` folder called ` profile.lang.yml`, an example would be `profile.jp.yml`
+2) Copy the contents of `profile.en.yml` to your new file
+3) Fill it like this:
+3) **Do not change my keys**, the keys are like `profile_help:`. Just replace the english with your text
+
+
+
+```yaml
+jp:
+  example_1: your shit goes here
+  
+  example_multiline: |
+  
+    if it is multiline do this
+    the | is very important if youre doing multiple lines, be sure and match my tabs
+    
+  example_2: you should be able to understand how this works
+```
+This format is called YAML. I highly recommend picking up a text editor that preserves my tabs and spacing. Notepad++ is good enough, if youre that invested in doing this of course. 
+
+
+To submit your translation, make a pull request or upload your file to pastebin and ping me on discord
---
+++ _old/localconf.py
@@ -0,0 +1,48 @@
+import i18n
+
+supported_lang = [
+    'en',
+    'jp',
+    'fr',
+    'de',
+    'es',
+    'zh-cn', #mandarin
+    'zh-tw', #cantonese
+]
+
+i18n.load_path.append('cogs/lib/locale/locale')
+i18n.load_path.append('cogs/lib/locale/locale/profile')
+#profile
+profile_help = i18n.t('profile.profile_help')
+level_help = i18n.t('profile.level_help')
+level_set = i18n.t('profile.level_set')
+level_error1 = i18n.t('profile.level_error1')
+level_error2 = i18n.t('profile.level_error2')
+level_error3 = i18n.t('profile.level_error3')
+level_error4 = i18n.t('profile.level_error4')
+weapon_help = i18n.t('profile.weapon_help')
+w1 = i18n.t('profile.w1')
+w2 = i18n.t('profile.w2')
+w3 = i18n.t('profile.w3')
+wep_error = i18n.t('profile.wep_error')
+rank_help = i18n.t('profile.rank_help')
+rank_set = i18n.t('profile.rank_set')
+rank_error1 = i18n.t('profile.rank_error1')
+rank_error2 = i18n.t('profile.rank_error2')
+league_power_help = i18n.t('profile.league_power_help')
+team_power = i18n.t('profile.team_power')
+pair_power = i18n.t('profile.pair_power')
+rank_error3 = i18n.t('profile.rank_error3')
+rank_error4 = i18n.t('profile.rank_error4')
+fc_help = i18n.t('profile.fc_help')
+fc_added = i18n.t('profile.fc_added')
+fc_error = i18n.t('profile.fc_error')
+db_error = i18n.t('profile.db_error')
+ign_help = i18n.t('profile.ign_help')
+ign_set = i18n.t('profile.ign_set')
+sr_help = i18n.t('profile.sr_help')
+loadout_help = i18n.t('profile.loadout_help')
+l1 = i18n.t('profile.l1')
+l2 = i18n.t('profile.l2')
+l3 = i18n.t('profile.l3')
+loadout_error = i18n.t('profile.loadout_error')
